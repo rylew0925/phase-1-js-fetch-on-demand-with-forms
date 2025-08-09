c8931ba9f7498486deb3351838f08be4b5230ec1
Richard <rlew0925@gmail.com>
2025-08-08 18:01:32 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ const init = () => {
     event.preventDefault();
     const input = document.querySelector("input#searchByID");
 
-    console.log(input.value);
-
-    fetch("http://localhost:3000/movies")
+    fetch(`http://localhost:3000/movies/${input.value}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        // LOG: (3) [{…}, {…}, {…}]
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
       });
   });
 };
